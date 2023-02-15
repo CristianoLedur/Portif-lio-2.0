55458cbcb19eca8e235f8b60877d9b482b8bd677
Cristiano Ledur <cristianoledur98@gmail.com>
2023-02-15 01:32:16 -0300

    first commit

---
+++ js/portifolio.js
@@ -0,0 +1,125 @@
+const portifolios = [
+  {
+    imageDesktop: 'assets/portifolio/00-desktop.png',
+    imageMobile: 'assets/portifolio/00-mobile.png',
+    linkPortifolio: 'https://psychedelic-book.surge.sh/',
+    imageAlt: 'Imagem do portifolio do jogo da memória'
+  },
+  {
+    imageDesktop: 'assets/portifolio/01-desktop.png',
+    imageMobile: 'assets/portifolio/01-mobile.png',
+    linkPortifolio: 'https://fantastic-question.surge.sh/',
+    imageAlt: 'Imagem do portifolio to-do list'
+  },
+  {
+    imageDesktop: 'assets/portifolio/02-desktop.png',
+    imageMobile: 'assets/portifolio/02-mobile.png',
+    linkPortifolio: 'https://probable-substance.surge.sh/',
+    imageAlt: 'Imagem do portifolio do jogo da velha'
+  },
+  {
+    imageDesktop: 'assets/portifolio/03-desktop.png',
+    imageMobile: 'assets/portifolio/03-mobile.png',
+    linkPortifolio: 'https://omniscient-wave.surge.sh/',
+    imageAlt: 'Imagem do portifolio utilizando JSON'
+  },
+  {
+    imageDesktop: 'assets/portifolio/04-desktop.png',
+    imageMobile: 'assets/portifolio/04-mobile.png',
+    linkPortifolio: 'https://industrious-act.surge.sh/',
+    imageAlt: 'Imagem do portifolio utilizando API'
+  },
+  {
+    imageDesktop: 'assets/portifolio/05-desktop.png',
+    imageMobile: 'assets/portifolio/05-mobile.png',
+    linkPortifolio: 'https://humorous-wind.surge.sh/',
+    imageAlt: 'Imagem do portifolio validando formulário'
+  }
+];
+
+let desktop = document.querySelector(".link-interface-desktop");
+let mobile = document.querySelector(".link-interface-mobile");
+let btn_left = document.querySelector("#btn-left");
+let btn_right = document.querySelector("#btn-right");
+let btn_submit = document.getElementById("submit-form");
+let positionArray = 0;
+const arrayLegnth = portifolios.length;
+let intervalId;
+
+btn_submit.addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementsByTagName('form')[0].submit();
+});
+
+// Incializa a pagina
+window.onload = function() {
+  checkWindowWidth();
+  document.querySelector('#screen-portifolio-desktop').src = portifolios[0].imageDesktop;
+  document.querySelector('#screen-portifolio-mobile').src = portifolios[0].imageMobile;
+  desktop.href = portifolios[0].linkPortifolio;
+  mobile.href = portifolios[0].linkPortifolio;
+}
+
+// Chama a função quando a tela for redimencionada
+window.onresize = function() {
+  checkWindowWidth();
+};
+
+// botão esquerdo portifólio
+btn_left.addEventListener("click", function() {
+  decrementaPosition(positionArray);
+  document.querySelector('#screen-portifolio-desktop').src = portifolios[positionArray].imageDesktop;
+  document.querySelector('#screen-portifolio-mobile').src = portifolios[positionArray].imageMobile;
+  desktop.href = portifolios[positionArray].linkPortifolio;
+  mobile.href = portifolios[positionArray].linkPortifolio;
+});
+
+// botão direito portifólio
+btn_right.addEventListener("click", function() {
+  incrementaPosition(positionArray);
+  document.querySelector('#screen-portifolio-desktop').src = portifolios[positionArray].imageDesktop;
+  document.querySelector('#screen-portifolio-mobile').src = portifolios[positionArray].imageMobile;
+  desktop.href = portifolios[positionArray].linkPortifolio;
+  mobile.href = portifolios[positionArray].linkPortifolio;
+});
+
+// função para alterar slides no portifólio
+function checkWindowWidth() {
+  // Obtém a largura da janela
+  const windowWidth = window.innerWidth;
+
+  // Verifica se a largura da janela é menor que 1250px
+  if (windowWidth < 1251) {
+    document.querySelector('#screen-portifolio-desktop').src = portifolios[positionArray].imageDesktop;
+    document.querySelector('#screen-portifolio-mobile').src = portifolios[positionArray].imageMobile;
+    desktop.href = portifolios[positionArray].linkPortifolio;
+    mobile.href = portifolios[positionArray].linkPortifolio;
+    incrementaPosition(positionArray);
+    intervalId = setInterval(checkWindowWidth, 5000);
+  } else {
+    clearInterval(intervalId);
+  }
+}
+
+// incrementa contador
+function incrementaPosition(item) {
+  if(item === arrayLegnth-1) {
+    return positionArray = 0;
+  }
+  return positionArray++;
+}
+
+// decrementa contador
+function decrementaPosition(item) {
+  if(item === 0) {
+    return positionArray = arrayLegnth-1;
+  }
+  return positionArray--;
+}
+
+// submit formulario
+document.getElementsByTagName("form")[0].addEventListener("submit", function(event){
+  event.preventDefault();
+});
+
+
